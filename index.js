7fed3d5e4f33cb69bff99a410f43851e5424daff
Joanna Preston <joannappreston@gmail.com>
2020-05-18 13:06:29 -0600

    initial commit. barebones database

---
+++ index.js
@@ -0,0 +1,9 @@
+// set up files: index.js, inquirer questions (class), schema.sql, seed.sql, SQL query functions (class)
+// initialie node and install packages - inquirer, mysql, console.table
+// create database and tables (3) - MySQL workbench and schema.sql
+// insert data to work with (seed.sql)
+// connect to mysqld server (index.js)
+// set up SQL queries in their own class
+// set up inquirer questions class
+// pull questions and queries together in index.js file 
+
