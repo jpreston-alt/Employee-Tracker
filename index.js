366fc6ca99ab21ef8a9bcd6d56975586c26ecf43
Joanna Preston <joannappreston@gmail.com>
2020-05-21 19:07:33 -0400

    Able to update employee role

--- index.js
+++ index.js
@@ -11,40 +11,41 @@ let roleQuery = Query.roleQuery;
 let QueryClass = Query.Query;
 
 function init() {
-    inquirer
-        .prompt(questions.toDoQuestion)
-        .then(data => {
-            switch (data.toDoType) {
-                case "View all Employees":
-                    employeeQuery.viewTable();
-                    break;
-                case "View all Departments":
-                    departmentQuery.viewTable();
-                    break;
-                case "View all Roles":
-                    roleQuery.viewTable();
-                    break;
-                case "Add Employee":
-                    employeeQuery.addToTable();
-                    break;
-                case "Add Department":
-                    departmentQuery.addToTable();
-                    break;
-                case "Add Role":
-                    roleQuery.addToTable();
-                    break;
-                case "Remove Employee":
-                    employeeQuery.deleteFromTable();
-                    break;
-                case "Remove Department":
-                    departmentQuery.deleteFromTable();
-                    break;
-                case "Remove Role":
-                    roleQuery.deleteFromTable();
-                    break;
-                default:
-                    db.end();
-            }
+    inquirer.prompt(questions.toDoQuestion).then(data => {
+        switch (data.toDoType) {
+            case "View all Employees":
+                employeeQuery.viewTable();
+                break;
+            case "View all Departments":
+                departmentQuery.viewTable();
+                break;
+            case "View all Roles":
+                roleQuery.viewTable();
+                break;
+            case "Add Employee":
+                employeeQuery.addToTable();
+                break;
+            case "Add Department":
+                departmentQuery.addToTable();
+                break;
+            case "Add Role":
+                roleQuery.addToTable();
+                break;
+            case "Remove Employee":
+                employeeQuery.deleteFromTable();
+                break;
+            case "Remove Department":
+                departmentQuery.deleteFromTable();
+                break;
+            case "Remove Role":
+                roleQuery.deleteFromTable();
+                break;
+            case "Update Employee Role":
+                employeeQuery.updateRole();
+                break;
+            default:
+                db.end();
+        }
         })
         .catch(err => {
             if (err) throw err;
@@ -56,37 +57,24 @@ init();
 // view table method
 QueryClass.prototype.viewTable = function() {
     this.findColVals();
-    db.query(
-        `Select * FROM ${this.table}`,
-        function (err, res) {
+    let query;
+
+    if (this.table == "employee") {
+        query = this.joinQuery;
+    } else {
+        query = `Select * FROM ${this.table}`;
+    };
+
+    db.query(query, function (err, res) {
             if (err) throw err;
+            console.log("\n");
             console.table(res);
+            console.log("\n");
             init();
         });
 };
 
-// add to table method
-QueryClass.prototype.addToTable = function() {
-    this.findColVals();
-    this.askQuestions().then(data => {
-        this.params = Object.values(data);
-        let table = this.table;
-        db.query(
-            `INSERT INTO ${this.table} (${this.columns}) VALUES (?);`,
-            [this.params],
-            function (err, res) {
-                if (err) throw err;
-                console.table(`New ${table} added!`);
-                init();
-        });
-    });
-};
-
-// updateTable() {
-
-// };
-
-// method for removing instance from table
+// delete method
 QueryClass.prototype.deleteFromTable = function () {
     this.findColVals();
     let choices = this.deleteQuestion.choices;
@@ -100,7 +88,7 @@ QueryClass.prototype.deleteFromTable = function () {
             [deleteVal],
             function (err, res) {
                 if (err) throw err;
-                console.table(`${type} deleted`);
+                console.log(`\n${type} deleted!\n`);
 
                 for (var i = 0; i < choices.length; i++) {
                     if (choices[i] == deleteVal) {
@@ -113,3 +101,84 @@ QueryClass.prototype.deleteFromTable = function () {
     });
 };
 
+employeeQuery.updateRole = function() {
+    inquirer.prompt(questions.updateEmpQ).then(data => {
+        console.log(data);
+        const { updateEmployee, updateRole } = data;
+        db.query(
+            `SELECT * FROM role`,
+            function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+                    if (updateRole === res[i].name) {
+                        db.query(
+                            `UPDATE employee SET role_id=${res[i].id} WHERE first_name="${updateEmployee}";`,
+                            function(err, res) {
+                                if (err) throw err;
+                                console.log(`\nEmployee Updated!\n`)
+                                init();
+                            }
+                        )
+                    }
+                }
+            }
+        )
+    });
+};
+
+QueryClass.prototype.addToTable = function () {
+    let table = this.table;
+    let columns = this.columns;
+    let params = [];
+    let otherTable;
+
+    if (table === "employee") {
+        otherTable = "role";
+    } else if (table === "role") {
+        otherTable = "department";
+    };
+
+    this.findColVals();
+
+    this.askQuestions().then(data => {
+        params.push(Object.values(data)[0]);
+        params.push(Object.values(data)[1]);
+
+        db.query(
+            `SELECT * FROM ${otherTable}`,
+            function (err, res) {
+                if (err) throw err;
+
+                for (var i = 0; i < res.length; i++) {
+                    if (Object.values(data)[2] == res[i].name) {
+                        params.push(res[i].id);
+                    };
+                };
+
+                db.query(
+                    `INSERT INTO ${table} (${columns}) VALUES (?);`,
+                    [params],
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log(`\nNew ${table} added!\n`);
+                        init();
+                    });
+            });
+    });
+};
+
+departmentQuery.addToTable = function () {
+    this.findColVals();
+    this.askQuestions().then(data => {
+        let params = Object.values(data);
+        let table = this.table;
+        db.query(
+            `INSERT INTO ${this.table} (${this.columns}) VALUES (?);`,
+            [params],
+            function (err, res) {
+                if (err) throw err;
+                console.log(`\nNew ${table} added!\n`);
+                init();
+            });
+    });
+};
